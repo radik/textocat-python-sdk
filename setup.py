b52d7122911632e89d9834e155b76d25d6e7227f
Radik Fattakhov <radikft@gmail.com>
2015-05-15 14:30:11 +0300

    bumped version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='textocat',
-    version='0.1.1',
+    version='0.1.2',
     description='Unofficial Textocat Python SDK',
     url='http://github.com/radik/textocat-python-sdk',
     author='Radik Fattakhov',
